f6ea0ca8cf387cce7431f3ddd565042386c52dc8
VimukthiShohanJay <vimukthi.shohan@gmail.com>
2025-03-01 00:44:52 +0530

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  semi: true,
+  printWidth: 120,
+};
